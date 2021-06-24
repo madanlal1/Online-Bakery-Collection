04ef53c784e756d8d1bfc322e0779225c397a8af
LS Madan <madanlal.suther@gmail.com>
2021-06-24 00:44:07 -0700

    'few'

---
+++ src/CakesData.js
@@ -0,0 +1,22 @@
+const CakesData = [
+
+    {image:"images/cake/cake16.jpg", title:"Chocolate Cake"},
+    {image:"images/cake/cake6.png", title:"Pound Cake"},
+    {image:"images/cake/cake5.png", title:"Fruit Cake"},
+    {image:"images/cake/cake8.png", title:"Strawberry Cake"},
+    {image:"images/cake/cake1.png", title:"Carrot Cake"},
+    {image:"images/cake/cake2.png", title:"Chocolate Cake"},
+    {image:"images/cake/cake3.jpg", title:"Chocolate  Cake"},
+    {image:"images/cake/cake4.jpg", title:"Chocolate Biscuit Cake"},
+    {image:"images/cake/cake7.jpg", title:"Cream Cake"},
+    {image:"images/cake/cake9.jpg", title:"Wedding Cake"},
+    {image:"images/cake/cake10.jpeg", title:"Tiger Cake"},
+    {image:"images/cake/cake11.jpeg", title:"Cup Cake"},
+    {image:"images/cake/cake12.jpeg", title:"Slim Cake"},
+    {image:"images/cake/cake13.jpeg", title:"Easter Cup Cake"},
+    {image:"images/cake/cake14.jpeg", title:"Halloween Cup Cake"},
+    {image:"images/cake/cake15.jpg", title:"Chocolate Cup Cake"}
+    
+]
+
+export default CakesData;
